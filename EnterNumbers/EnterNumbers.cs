1e55d1c9dbe13ca688fde677022cba3b58f38f7c
orlindraganov <orlindraganov@gmail.com>
2016-05-29 13:48:11 +0300

    Done tasks 1 & 2

---
+++ EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,58 @@
+﻿namespace EnterNumbers
+{
+    using System;
+    using System.Linq;
+
+
+
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int[] numbers = new int[10];
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    numbers[i] = int.Parse(Console.ReadLine());
+                }
+                PrintIfSorted(1, 100, numbers);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+
+        }
+
+        static void PrintIfSorted(int start, int end, int[] numbers)
+        {
+            try
+            {
+                if (!IsSorted(numbers) || numbers.Any(x => x < start) || numbers.Any(x => x > end))
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine(start + " < " + string.Join(" < ", numbers) + " < " + end);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        static bool IsSorted(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] >= numbers[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ SquareRoot/SquareRoot.cs
@@ -0,0 +1,50 @@
+﻿namespace SquareRoot
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+
+    class SquareRoot
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            try
+            {
+                double number = GetPositiveNumber();
+                Console.WriteLine("{0:F3}", Math.Sqrt(number));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+        static double GetPositiveNumber()
+        {
+            double number;
+            try
+            {
+                number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new FormatException();
+                }
+                return number;
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Invalid number");
+            }
+            catch (ArgumentNullException)
+            {
+                throw new FormatException("Invalid number");
+            }
+        }
+
+    }
+}
